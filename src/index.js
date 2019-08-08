46320b950a5e48f8d480457cb2550a521932e3b8
litufu <285212818@qq.com>
2019-08-08 10:28:37 +0800

    add apollo client

--- src/index.js
+++ src/index.js
@@ -4,21 +4,62 @@ import ReactDOM from 'react-dom';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
+import { ApolloLink, split } from 'apollo-link'
+import { getMainDefinition } from 'apollo-utilities'
+import { ApolloProvider } from '@apollo/react-hooks';
+import { WebSocketLink } from 'apollo-link-ws'
 
-import { ApolloProvider, useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
-
-import './index.css';
-import App from './App';
+import { AUTH_TOKEN } from './constant'
+import './styles/index.css'
+import App from './components/App'
 import * as serviceWorker from './serviceWorker';
 
-const cache = new InMemoryCache();
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      Authorization: `Bearer ${localStorage.getItem(AUTH_TOKEN)}`,
+    },
+  },
+})
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  const authorizationHeader = token ? `Bearer ${token}` : null
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader,
+      'client-name': 'Space Explorer [web]',
+      'client-version': '1.0.0',
+    },
+  })
+  return forward(operation)
+})
+
+const httpLinkAuth  = middlewareAuthLink.concat(httpLink)
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLinkAuth,
+)
+
+const client = new ApolloClient({
+  link: ApolloLink.from([link]),
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+})
 
 
 ReactDOM.render(
     <ApolloProvider client={client}>
+      <App />
     </ApolloProvider>,
     document.getElementById('root'),
   );
