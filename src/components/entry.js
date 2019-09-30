8857db8ffb68146248ce2b60c1ed4775c99bdd65
litufu <285212818@qq.com>
2019-09-30 13:35:29 +0800

    添加调整分录

--- src/components/entry.js
+++ src/components/entry.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
 import {sum } from '../utils'
 import MaterialTable from 'material-table';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import SelectSubject from './select-subject'
 import SelectAuxiliary from './select-auxiliary'
+
+const ADD_ADUIT_ADJUSTMENT = gql`
+  mutation AddAduitAdjustment($projectId: String!,$record:String!) {
+    addAduitAdjustment(projectId: $projectId,record:$record) 
+  }
+`;
 
 const useStyles = makeStyles(theme => ({
     button: {
@@ -35,15 +41,15 @@ function sumArray(arr,content){
 export default function Entry(props) {
   const classes = useStyles();
   const {projectId} = props
-  
   const [state, setState] = React.useState({
     columns: [
       {
         title: '摘要',
-        field: 'discription',
+        field: 'description',
         cellStyle: {
             width:300,
-        }
+        },
+        initialEditValue:"",
       },
       {
         title: '会计科目',
@@ -55,32 +61,53 @@ export default function Entry(props) {
       {
         title: '币种',
         field: 'currency_type',
+        initialEditValue:"RMB",
       },
       {
         title: '原币金额',
         field: 'foreign_currency',
+        initialEditValue:"0.00",
+        type:"numeric"
       },
       {
         title: '借方',
         field: 'debit',
+        initialEditValue:"0.00",
+        type:"numeric"
       },
       {
         title: "贷方",
         field: 'credit',
+        initialEditValue:"0.00",
+        type:"numeric"
       },
       {
         title: "辅助核算",
         field: 'auxiliary',
         editComponent: props => (
           <SelectAuxiliary value={props.value} onChange={props.onChange} projectId={projectId}/>
-        )
+        ),
+        initialEditValue:"",
       },
 
     ],
-    data: [
-        {discription:"",subject:"",currency_type:"RMB",foreign_currency:"",debit:0.00,credit:0.00,auxiliary:""},
-    ],
+    data: [],
   });
+  const [addAduitAdjustment] = useMutation(
+    ADD_ADUIT_ADJUSTMENT,
+    {
+      onCompleted({ addAduitAdjustment }) {
+        if(addAduitAdjustment){
+          alert("保存完成")
+          setState({ ...state, data:[] });
+        }else{
+          alert("保存失败")
+        }
+      }
+    }
+    );
+  
+  
 
   return (
       <div>
@@ -123,12 +150,23 @@ export default function Entry(props) {
       }}
     />
     <div>
-    <Button variant="contained" className={classes.button}>
+    <Button 
+    variant="contained"
+    className={classes.button}
+    onClick={()=>{
+      if((sumArray(state.data,"debit")-sumArray(state.data,"credit"))>0.00){
+        alert("借贷方不相等")
+        return
+      }
+      if(state.data.length===1){
+        alert("不能只输入一行分录")
+        return
+      }
+      addAduitAdjustment({ variables: { projectId: props.projectId,record:JSON.stringify(state.data) } });
+    }}
+    >
         保存
       </Button>
-    <Typography variant="h6" gutterBottom>
-    {`借方合计数${sumArray(state.data,"debit")},贷方合计数${sumArray(state.data,"credit")}`}
-    </Typography> 
     </div>
     </div>
   );
